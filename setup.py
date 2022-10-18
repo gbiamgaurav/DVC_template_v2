8ded1c99aca400a18f9ce20f402ef0d500d41e38
gbgaurav10 <gaurav.bhattacharya10@outlook.com>
2022-10-18 19:32:39 +0530

    changes done

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "<github repo name>" # github repo name
-AUTHOR_USER_NAME = "c17hawke" # update as per your need
-SRC_REPO = "<change me>" # example sklearn etc
+REPO_NAME = "DVC_template_v2" # github repo name
+AUTHOR_USER_NAME = "gbiamgaurav" # update as per your need
+SRC_REPO = "DVC_template_v2" # example sklearn etc
 LIST_OF_REQUIREMENTS = [] # core dependencies
 
 
@@ -18,10 +18,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="gaurav.bhattacharya10@gmail.com",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
 )
