51f8b860fcb88d98abdc615ac6d751f8e1be6120
gbgaurav10 <gaurav.bhattacharya10@outlook.com>
2022-10-18 19:38:13 +0530

    done

--- init_setup.sh
+++ init_setup.sh
@@ -1,7 +1,7 @@
 echo [$(date)]: "START"
 export _VERSION_=3.8
-echo [$(date)]: "creating environment with python 3.8"
-conda create --prefix ./env python=3.8 -y 
+echo [$(date)]: "creating environment with python ${_VERSION_}"
+conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
